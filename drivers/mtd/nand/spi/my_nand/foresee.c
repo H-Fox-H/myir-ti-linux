bfa5da985afb97a08da1c009b07a75f128f02ddb
H-Fox-H <13669687250@163.com>
2024-11-19 08:01:03 +0800

    update 24111900

--- drivers/mtd/nand/spi/my_nand/foresee.c
+++ drivers/mtd/nand/spi/my_nand/foresee.c
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2020 Grandstream Networks, Inc
+ * Copyright (c) 2022 exceet electronics GmbH
  *
  * Authors:
- *	Carl <xjxia@grandstream.cn>
+ *	nan.li <nan.li@semidrive.com>
+ *
  */
 
-#ifndef __UBOOT__
 #include <linux/device.h>
 #include <linux/kernel.h>
-#endif
+#include <linux/bitops.h>
 #include <linux/mtd/spinand.h>
 
-
 #define SPINAND_MFR_FORESEE		0xCD
-#define FORESEE_STATUS_ECC_NO_BITFLIPS	    (0 << 4)
-#define FORESEE_STATUS_ECC_1_BITFLIPS	    (1 << 4)
-#define FORESEE_ECC_UNCOR_ERROR10           (2 << 4)
-#define FORESEE_ECC_UNCOR_ERROR11           (3 << 4)
-#define STATUS_ECC_MASK		GENMASK(5, 4)
 
+#define F35SQA001G_SI_MASK	GENMASK(5, 4)
+#define F35SQA001G_SES_MASK	GENMASK(3, 0)
+#define F35SQA001G_MAX_SES	4
+static u64 f35sqa001g_ses_reg[] = {0x80, 0x84, 0x88, 0x8c};
 
 static SPINAND_OP_VARIANTS(read_cache_variants,
+		SPINAND_PAGE_READ_FROM_CACHE_QUADIO_OP(0, 2, NULL, 0),
 		SPINAND_PAGE_READ_FROM_CACHE_X4_OP(0, 1, NULL, 0),
+		SPINAND_PAGE_READ_FROM_CACHE_DUALIO_OP(0, 1, NULL, 0),
 		SPINAND_PAGE_READ_FROM_CACHE_X2_OP(0, 1, NULL, 0),
 		SPINAND_PAGE_READ_FROM_CACHE_OP(true, 0, 1, NULL, 0),
 		SPINAND_PAGE_READ_FROM_CACHE_OP(false, 0, 1, NULL, 0));
@@ -32,64 +32,93 @@ static SPINAND_OP_VARIANTS(write_cache_variants,
 		SPINAND_PROG_LOAD(true, 0, NULL, 0));
 
 static SPINAND_OP_VARIANTS(update_cache_variants,
-		SPINAND_PROG_LOAD_X4(true, 0, NULL, 0),
-		SPINAND_PROG_LOAD(true, 0, NULL, 0));
+		SPINAND_PROG_LOAD_X4(false, 0, NULL, 0),
+		SPINAND_PROG_LOAD(false, 0, NULL, 0));
 
-static int fsxxndxxg_ooblayout_ecc(struct mtd_info *mtd, int section,
-				   struct mtd_oob_region *region)
-{
-	if (section > 3)
-		return -ERANGE;
-
-	/* ECC is not user accessible */
-	region->offset = 0;
-	region->length = 0;
-
-	return 0;
-}
-
-static int fsxxndxxg_ooblayout_free(struct mtd_info *mtd, int section,
+static int f35sqa001g_ooblayout_ecc(struct mtd_info *mtd, int section,
 				    struct mtd_oob_region *region)
 {
-	printf("Debug: Foresee layout: ");
 	if (section > 3)
 		return -ERANGE;
 
-	/*
-	 * No ECC data is stored in the accessible OOB so the full 16 bytes
-	 * of each spare region is available to the user. Apparently also
-	 * covered by the internal ECC.
-	 */
-	if (section) {
-		region->offset = 16 * section;
-		region->length = 16;
-	} else {
-		/* First byte in spare0 area is used for bad block marker */
-		region->offset = 2;
-		region->length = 14;
-	}
+	region->offset = (16 * section) + 8;
+	region->length = 8;
 
 	return 0;
 }
 
-static const struct mtd_ooblayout_ops fsxxndxxg_ooblayout = {
-	.ecc = fsxxndxxg_ooblayout_ecc,
-	.rfree = fsxxndxxg_ooblayout_free,
-};
-
-static int fsxxndxxg_ecc_get_status(struct spinand_device *spinand, u8 status)
+static int f35sqa001g_ooblayout_free(struct mtd_info *mtd, int section,
+				     struct mtd_oob_region *region)
 {
+	if (section > 3)
+		return -ERANGE;
+
+	/* Reserve 1 bytes for the BBM. */
+	region->offset = (16 * section) + 1;
+	region->length = 7;
+
+	return 0;
+}
+
+static int f35sqa001g_ecc_get_ses(struct spinand_device *spinand,
+				  u8 *status)
+{
+	int ret, i, num;
+	u8 ses[4] = {0};
+	struct spi_mem_op op[] = {
+		SPINAND_GET_FEATURE_OP(f35sqa001g_ses_reg[0], &ses[0]),
+		SPINAND_GET_FEATURE_OP(f35sqa001g_ses_reg[1], &ses[1]),
+		SPINAND_GET_FEATURE_OP(f35sqa001g_ses_reg[2], &ses[2]),
+		SPINAND_GET_FEATURE_OP(f35sqa001g_ses_reg[3], &ses[3]),
+	};
+
+	num = 0;
+	for (i = 0; i < F35SQA001G_MAX_SES; i++) {
+		ret = spi_mem_exec_op(spinand->spimem, &op[i]);
+		if (ret) {
+			pr_err("%s: get sec_%d es fail\n", __func__, i);
+			return ret;
+		}
+		if (ses[i] & F35SQA001G_SES_MASK == 0x1)
+			num++;
+		if (ses[i] & F35SQA001G_SES_MASK & 0x2)
+			pr_debug("sec %d has more than 1-bit err\n", i);
+	}
+
+	*status = num;
+	pr_debug("Detect %d sectors 1-bit err and be corrected\n", num);
+
+	return 0;
+}
+
+static int f35sqa001g_ecc_get_status(struct spinand_device *spinand,
+				     u8 status)
+{
+	int ret;
+	u8 ses;
+
 	switch (status & STATUS_ECC_MASK) {
-	case FORESEE_STATUS_ECC_NO_BITFLIPS:
+	case STATUS_ECC_NO_BITFLIPS:
 		return 0;
 
-	case FORESEE_STATUS_ECC_1_BITFLIPS:
-		return 1;
+	case STATUS_ECC_HAS_BITFLIPS:
+		/*
+		 * Read sector status register to determine a more fine grained
+		 * bit error status
+		 */
+		ret = f35sqa001g_ecc_get_ses(spinand, &ses);
+		if (ret)
+			return ret;
 
-	case FORESEE_ECC_UNCOR_ERROR10:
-	case FORESEE_ECC_UNCOR_ERROR11:	
+		/*
+		 * 1-bit err was detected in one or more sector and corrected
+		 */
+		/* bits sorted these regs Sector ECC Status 0~3 */
+		return ses;
+
+	case STATUS_ECC_UNCOR_ERROR:
 		return -EBADMSG;
-	
+
 	default:
 		break;
 	}
@@ -97,90 +126,31 @@ static int fsxxndxxg_ecc_get_status(struct spinand_device *spinand, u8 status)
 	return -EINVAL;
 }
 
-static const struct spinand_info foresee_spinand_table[] = {
-	SPINAND_INFO("F35SQA512M",
-		     0x70,
-		     NAND_MEMORG(1, 2048, 64, 64, 512, 10, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
-	SPINAND_INFO("F35SQA001G",
-		     0x71,
-		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
-	SPINAND_INFO("F35SQA002G",
-		     0x72,
-		     NAND_MEMORG(1, 2048, 64, 64, 2048, 40, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
-	SPINAND_INFO("F35UQA512M",
-		     0x60,
-		     NAND_MEMORG(1, 2048, 64, 64, 512, 10, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
-	SPINAND_INFO("F35UQA001G",
-		     0x61,
-		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
-	SPINAND_INFO("F35UQA002G",
-		     0x62,
-		     NAND_MEMORG(1, 2048, 64, 64, 2048, 40, 1, 1, 1),
-		     NAND_ECCREQ(1, 512),
-		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
-					      &write_cache_variants,
-					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT,
-		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, fsxxndxxg_ecc_get_status)),
+static const struct mtd_ooblayout_ops f35sqa001g_ooblayout = {
+	.ecc = f35sqa001g_ooblayout_ecc,
+	.free = f35sqa001g_ooblayout_free,
 };
 
-static int foresee_spinand_detect(struct spinand_device *spinand)
-{
-	u8 *id = spinand->id.data;
-	int ret;
-	/*
-	 * For Foresee NANDs, There is an address byte needed to shift in before IDs
-	 * are read out, so the first byte in raw_id is dummy.
-	 */
-	if (id[1] != SPINAND_MFR_FORESEE)
-		return 0;
-
-	ret = spinand_match_and_init(spinand, foresee_spinand_table,
-				     ARRAY_SIZE(foresee_spinand_table),
-				     id[2]);
-	printf("Mfg. is 0x%02X, device is 0x%02X match and init status 0x%02X\n", id[1], id[2], ret);
-	if (ret)
-		return ret;
-
-	return 1;
-}
+static const struct spinand_info foresee_spinand_table[] = {
+	SPINAND_INFO("F35SQA001G",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x71),
+		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&f35sqa001g_ooblayout,
+				     f35sqa001g_ecc_get_status)),
+};
 
 static const struct spinand_manufacturer_ops foresee_spinand_manuf_ops = {
-	.detect = foresee_spinand_detect,
 };
 
 const struct spinand_manufacturer foresee_spinand_manufacturer = {
 	.id = SPINAND_MFR_FORESEE,
-	.name = "foresee",
+	.name = "Foresee",
+	.chips = foresee_spinand_table,
+	.nchips = ARRAY_SIZE(foresee_spinand_table),
 	.ops = &foresee_spinand_manuf_ops,
 };
